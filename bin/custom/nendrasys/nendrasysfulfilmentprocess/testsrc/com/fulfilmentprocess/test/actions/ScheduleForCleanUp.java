915bd578bc1da245f160b174b72ae4789bbde5b1
saiakshith <vangarusaiakshith98@gmail.com>
2023-07-05 16:19:53 +0530

    Add /hybris/bin/custom/ & /hybris/bin/config/(local.properties | loaclextensions.xml)

---
+++ bin/custom/nendrasys/nendrasysfulfilmentprocess/testsrc/com/fulfilmentprocess/test/actions/ScheduleForCleanUp.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.fulfilmentprocess.test.actions;
+
+import com.fulfilmentprocess.actions.order.ScheduleForCleanUpAction;
+
+
+/**
+ * Test counterpart for {@link ScheduleForCleanUpAction}
+ */
+public class ScheduleForCleanUp extends TestActionTemp
+{
+	//EMPTY
+}
